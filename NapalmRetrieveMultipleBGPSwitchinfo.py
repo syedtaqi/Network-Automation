3870bc96ce90a2df45bdc6c11868376010d2be1b
syedtaqi <raza.taqi4@gmail.com>
2020-06-15 23:04:10 -0400

    Retrieve bgp information from multiple devices

---
+++ NapalmRetrieveMultipleBGPSwitchinfo.py
@@ -0,0 +1,20 @@
+import json
+from napalm import get_network_driver
+
+#Retrieving BGP information from multiple switches
+listBGP_ = [192.168.122.70, 
+            192.168.122.72, 
+            192.168.122.74, 
+            192.168.122.76,
+            192.168.122.78
+]
+for ip_ in listBGP:
+   print ( "connecting to " + str(ip_))
+   driver = get_network_driver('ios')
+   router_bgp = driver (ip_, 'taqi', 'cisco') 
+   router_bgp.open()
+
+   bgp_neighbors = router_bgp.get_bgp_neighbors()
+   print (json.dumps(bgp_neighbors, indent=3))
+
+    router_bgp.close()
