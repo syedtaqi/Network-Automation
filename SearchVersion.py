fe4238aabe018cd8264d1bc23e5e352e07d398c7
syedtaqi <raza.taqi4@gmail.com>
2020-06-17 14:03:18 -0400

    Update SearchVersion.py

--- SearchVersion.py
+++ SearchVersion.py
@@ -1,4 +1,21 @@
-Code by Taqi RazaSe
+Code by Taqi Raza
+#search for c1941 version router. if it is then #print the statement
+from netmiko import ConnectHandler
+
+cisco_router = {
+    'device_type': 'cisco_ios',
+    'host':   '10.10.10.10',
+    'username': 'taqi',
+    'password': 'cisco',
+
+}
+
+net_connect = ConnectHandler(**cisco_router)
+output = net_connect.send_command('show version')
+print (output)
+
+version_output = output.find('1941')
+
 if version_output> 0 :
      c1941 = True
 else:
