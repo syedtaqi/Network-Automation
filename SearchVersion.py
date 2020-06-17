7aafe7de7e776866a155c007b6e254a0e727c65c
syedtaqi <raza.taqi4@gmail.com>
2020-06-17 14:02:29 -0400

    Search Routers version

---
+++ SearchVersion.py
@@ -0,0 +1,15 @@
+Code by Taqi RazaSe
+if version_output> 0 :
+     c1941 = True
+else:
+     c1941 = False
+
+router = True
+
+if router:
+    if c1941:
+        print (' this is a 1941 router')
+    else: 
+        print( ' This is a router, but not 1941 router')
+else:
+    print ( ' Not a router ' )
